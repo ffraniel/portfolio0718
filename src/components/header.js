1db6cdb540872794824c160a663abb90b8ebb179
ffraniel <francis.whitehead@googlemail.com>
2018-07-28 18:50:25 +0100

    switched A for Links

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './header.css';
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header>
     <nav>
-      <a className="home-nav" href="#">Home</a>
-      <a className="portfolio-nav" href="#">Portfolio</a>
+      <Link className="home-nav" href="#">Home</Link>
+      <Link className="portfolio-nav" href="#">Portfolio</Link>
       <a className="github-nav" href="https://github.com/ffraniel/">Github</a>
       <a className="linkedin-nav" href="https://www.linkedin.com/in/francis-whitehead-394023155/">LinkedIn</a>
     </nav>
