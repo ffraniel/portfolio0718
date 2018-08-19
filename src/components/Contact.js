6064fb23fdaadfaef61e7069d275bb5d9cea5ec6
ffraniel <francis.whitehead@googlemail.com>
2018-08-19 17:42:26 +0100

    simplified contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,21 +5,8 @@ import './Contact.css';
 const Contact = () => (
     <footer className="contact">
         <h3>Contact</h3>
-        <p>To contact me for work head over to LinkedIn or just type in your email below and i'll get back to you.</p>
-        <div className="message-sent">
-            <h3>Message sent!</h3>
-        </div>
-        <div className="invalid-email">
-            <h3>Please include a valid email address.</h3>
-        </div>
-        <div className="no-message">
-            <h3>The message space was left blank. Please write something before sending.</h3>
-        </div>
-        <form id="contact-form">
-            <input type="text" placeholder="  email" id="email" required />
-            <input type="text" placeholder="  message" id="message" required />
-            <input type="submit" />
-        </form>
+        <p>To contact me for work head over to LinkedIn.</p>
+        <a href="https://www.linkedin.com/in/francis-whitehead-394023155/" className="contact-linkedin">Click to go to LinkedIn</a>
     </footer>
 )
 
