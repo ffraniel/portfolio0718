2b411a4b66c64b2acdfcff0ea0092072cc2b4d56
ffraniel <francis.whitehead@googlemail.com>
2018-08-19 18:30:27 +0100

    reverted header 'Home' button

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => (
   <header>
     <nav>
-      <Link className="home-nav" to="/">franwebdev.uk</Link>
+      <Link className="home-nav" to="/">Home</Link>
       <Link className="portfolio-nav" to="/portfolio/">Portfolio</Link>
       <a className="github-nav" href="https://github.com/ffraniel/">Github</a>
       <a className="linkedin-nav" href="https://www.linkedin.com/in/francis-whitehead-394023155/">LinkedIn</a>
